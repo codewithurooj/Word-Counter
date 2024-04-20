6ece216cac4fe937995387d84cd65fc6bded0267
codewithurooj <fatimaurooj152@gmail.com>
2024-04-20 10:34:43 -0700

    Adding Word Counter Assignment

---
+++ main.js
@@ -0,0 +1,21 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Display a welcome message
+console.log(chalk.bold.cyanBright("/n/t/tCode with urooj Word Counter!"));
+console.log("=".repeat(60));
+//Ask the user to enter a sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence abcd ",
+    }
+]);
+//Triming the sentence and spliting the words based on spaces
+let words = answers.sentence.trim().split(" ");
+//Anylsis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("-Sentence words:"));
+console.log(words);
+console.log(chalk.bold(`/n - word count:${chalk.bold.redBright(words.length)}`));
